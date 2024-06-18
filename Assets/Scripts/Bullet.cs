68d20bf57da965dcde7f29557b393e100a1f01ec
Chanhwi Lee <chanhwi.lee6@gmail.com>
2024-06-18 20:31:26 +0900

    ✨ [B50] :보스 미사일, 보 바위 만들기

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -6,10 +6,11 @@ public class Bullet : MonoBehaviour
 {
     public int damage;
     public bool isMelee;
+    public bool isRock;
 
     void OnCollisionEnter(Collision other)
     {
-        if (other.gameObject.tag == "Floor")
+        if (!isRock && other.gameObject.tag == "Floor")
         {
             Destroy(gameObject, 3);
         }
